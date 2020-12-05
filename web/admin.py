6e107d86edc85e1a3c466a204b8feae6d8a99fad
elahe sems <olenasems88@gmail.com>
2020-12-05 12:35:08 +0300

    "test"

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
-
+from web.models import *
 # Register your models here.
+
+class ModifiedSlider(admin.ModelAdmin):
+    list_display = ('title1','title2','title3','picture')# extra fields in the admin
+    list_filter = ('title1',) #filters
+    search_fields = ('title1',) #bu alanlar üzerinde search yapılacaktır
+    #date_hierarchy = 'title1' #navigate quickly through a date hierarchy.
+    ordering = ['id'] #order the list by 'appStatus' and then 'appDate'
+    #prepopulated_fields = {'slugName': ('title2',)} # write slug of the given name automatically
+class ModifiedProduct(admin.ModelAdmin):
+    list_display = ('name','price','currency','inDate','status',)# extra fields in the admin
+    list_filter = ('price','shoesSize','size',) #filters
+    search_fields = ('name',) #bu alanlar üzerinde search yapılacaktır
+    date_hierarchy = 'inDate' #navigate quickly through a date hierarchy.
+    ordering = ['id'] #order the list by 'appStatus' and then 'appDate'
+
+class ModifiedDesigner(admin.ModelAdmin):
+    list_display = ('name','lastname',)# extra fields in the admin
+    list_filter = ('name','lastname','email',) #filters
+    search_fields = ('name','lastname',) #bu alanlar üzerinde search yapılacaktır
+    # date_hierarchy = 'inDate' #navigate quickly through a date hierarchy.
+    ordering = ['id'] #order the list by 'appStatus' and then 'appDate'
+
+admin.site.register(Sliders,ModifiedSlider)
+admin.site.register(Product,ModifiedProduct)
+admin.site.register(Designer,ModifiedDesigner)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,91 @@
 from django.db import models
-
+from multiselectfield import MultiSelectField
 # Create your models here.
+
+class Sliders(models.Model):
+    title1 = models.CharField(max_length=100, null=True,blank=True, verbose_name='en ust sırada yazılacak')
+    title2 = models.CharField(max_length=100, null=True,blank=True,verbose_name='ortada yazılacak')
+    title3 = models.CharField(max_length=100, null=True,blank=True,verbose_name='en alt')
+    link = models.URLField(verbose_name='website',null=True,blank=True)
+    picture = models.ImageField('resim seçiniz',null=True,blank=True)
+    def __str__(self):
+        return self.title1
+    class Meta:
+        verbose_name_plural = "sliderler"
+
+
+class Designer(models.Model):
+    name = models.CharField(max_length=100,verbose_name='ad')
+    lastname = models.CharField(max_length=100,verbose_name='soyad')
+    telephone = models.CharField(max_length=100,verbose_name='telefon')
+    email = models.EmailField(max_length=100,verbose_name='eposta')
+    def __str__(self):
+        return str(self.name) + ' ' + str(self.lastname)
+    class Meta:
+        verbose_name_plural = "TaSaRıMCı"
+class Product(models.Model):
+    currencyType=(
+        ('tl','TL'),
+        ('usd','USD'),
+        ('euro','EURO')
+    )
+    categoryType = (
+        ('kadin', 'kadin'),
+        ('erkek', 'erkek'),
+        ('elektronik', 'elektronik'),
+        ('sport', 'sport'),
+        ('aksesuar', 'aksesuar'),
+        ('makyaj', 'makyaj'),
+    )
+    shoesSizeChoices = (
+        ('36', '36'),
+        ('37', '37'),
+        ('38', '38'),
+        ('39', '39'),
+        ('40', '40'),
+        ('41', '41'),
+        ('42', '42'),
+        ('43', '43'),
+        ('44', '44')
+    )
+    sizeChoices = (
+        ('xs', 'xs'),
+        ('s', 's'),
+        ('m', 'm'),
+        ('l', 'l'),
+        ('xl', 'xl'),
+        ('xxl', 'xxl'),
+        ('xxxl', 'xxxl')
+
+    )
+    colorChoices = (
+        ('red', 'red'),
+        ('pink', 'pink'),
+        ('orange', 'orange'),
+        ('yellow', 'yellow'),
+        ('purple', 'purple'),
+        ('green', 'green'),
+        ('blue', 'blue'),
+        ('brown', 'brown'),
+        ('black', 'black'),
+
+    )
+
+
+    name = models.CharField(max_length=100,verbose_name='ad')
+    price = models.IntegerField(max_length=100,verbose_name='fiyat')
+    currency = models.CharField(max_length=5,choices=currencyType, default='tl',verbose_name='kur')
+    picture = models.ImageField('resim seçiniz', null=True, blank=True)
+    designedby = models.ForeignKey(Designer,on_delete=models.CASCADE,verbose_name='tasarımcı', null=True, blank=True)
+    category = MultiSelectField(choices=categoryType,verbose_name='kategoriler',null=True,blank=True)
+    shoesSize = MultiSelectField(choices=shoesSizeChoices,verbose_name='ayakkabı numarası',null=True,blank=True)
+    size = MultiSelectField(choices=sizeChoices,verbose_name='size',null=True,blank=True)
+    color = MultiSelectField(choices=colorChoices,verbose_name='renk',null=True,blank=True)
+    inDate = models.DateField(auto_now_add=True,null=True,blank=True,verbose_name='giriş tarihi')
+    status = models.BooleanField(default=True,verbose_name='gösterilsin mi?')
+
+
+    class Meta:
+        verbose_name_plural = "Ürünler"
+
+
